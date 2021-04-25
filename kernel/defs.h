e49da5bdd17708c2b1d9097030fc1977bd3309d1
noy.aviv <noy.aviv@servicenow.com>
2021-04-25 23:18:34 +0300

    pric.h and proc.c changes

--- kernel/defs.h
+++ kernel/defs.h
@@ -106,6 +106,9 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 uint            sigprocmask (uint); // 2.1.3 Updating the process signal mask **make sure that we don't need to remove the variable name**
+int             sigaction (int, const struct sigaction *, struct sigaction *); 
+
+
 
 // swtch.S
 void            swtch(struct context*, struct context*);
